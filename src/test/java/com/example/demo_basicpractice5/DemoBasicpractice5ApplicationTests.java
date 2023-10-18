781d52e29a1a6bda852a840821a971827dc182b4
user <example@gmail.com>
2023-10-18 15:59:32 +0000

    Application Commit

---
+++ src/test/java/com/example/demo_basicpractice5/DemoBasicpractice5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_basicpractice5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBasicpractice5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
